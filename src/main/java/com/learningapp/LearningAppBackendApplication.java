6588d347d1b0f4626c8deec0f7bd5ac847dca4af
yacinebengharsallah <yassine.bengharsallah@esprit.tn>
2023-11-27 10:33:28 +0100

    first commit

---
+++ src/main/java/com/learningapp/LearningAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learningapp/LearningAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
